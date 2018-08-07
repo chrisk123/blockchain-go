4840655d560ecc3dec6532a7289e1e456d2589a3
Chris Kuan <chris.kuan@hotmail.com>
2018-08-07 03:33:05 -0700

    added helper method for chunking range of values into approximately equal-sized chunks; some work on work_queue

--- blockchain/mining.go
+++ blockchain/mining.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "fmt"
+
 type miningWorker struct {
 	// TODO. Should implement work_queue.Worker
 }
@@ -8,6 +10,12 @@ type miningWorker struct {
 type MiningResult struct {
 	Proof uint64 // proof-of-work value, if found.
 	Found bool   // true if valid proof-of-work was found.
+}
+
+type Chunk struct {
+	Size  uint64
+	Start uint64
+	End   uint64
 }
 
 // Mine the range of proof values, by breaking up into chunks and checking
@@ -21,23 +29,37 @@ func (blk Block) MineRange(start uint64, end uint64, workers uint64, chunks uint
 
 	var mineChannel = make(chan uint64, workers)
 	//Split mining range [start,end] into chunks and calculate concurrently
+	chunked := CalcChunks(start,end,chunks)
 
-	//Mine proof values in range [start, end]
-	for i := start; i < end; i++ {
 
-		//Assign block the next proof value and check validity of hash
-		miningBlk.Proof = i
-		miningBlk.CalcHash()
-
-		//Check if proof results in a valid hash and return result if so
-		if miningBlk.ValidHash() {
-			miningResult.Proof = i
-			mineChannel <- miningResult.Proof
-			miningResult.Found = true
-			return miningResult
+	//While Jobs in Queue exist, pass chunk down channel for workers to hash 
+		go func() {
+			//Pass chunked[i] range of values to the job queue
+			chunked[:]
 		}
-	}
+	
+	
+	// for i := 0; i < int(chunks-1); i++ {
+	// 	next_chunk :=
+	// }
+	// jobChunks :=
 
+	//Slowest situation, 1 worker mining entire range
+	if workers == 1 && chunks == 1 {
+		for i := start; i < end; i++ {
+			//Assign block the next proof value and check validity of hash
+			miningBlk.Proof = i
+			miningBlk.CalcHash()
+			//Check if proof results in a valid hash and return result if so
+			if miningBlk.ValidHash() {
+				miningResult.Proof = i
+				mineChannel <- miningResult.Proof
+				miningResult.Found = true
+				return miningResult
+			}
+		}
+
+	}
 	// blk.SetProof(res)
 	return miningResult
 }
@@ -53,4 +75,42 @@ func (blk *Block) Mine(workers uint64) bool {
 		blk.SetProof(mr.Proof)
 	}
 	return mr.Found
+}
+
+//CalcChunks function: Helper method to divide range into approximately equal-sized chunks
+//Returns an array of Chunk structs (.Size, .Start, .End)
+func CalcChunks(start, end, chunks uint64) []Chunk {
+	proofRange := end - start
+	avgChunkSize := proofRange / chunks
+	fmt.Printf("AvgChunkSize %v\n", avgChunkSize)
+	remainder := proofRange % chunks
+	fmt.Printf("Remainder %v\n", remainder)
+	chunk := make([]Chunk, chunks)
+
+	for i := uint64(0); i < chunks; i++ {
+		if i == 0 {
+			chunk[i].Size = avgChunkSize
+			chunk[i].Start = i * avgChunkSize
+			chunk[i].End = i*avgChunkSize + avgChunkSize
+			fmt.Printf("Chunk[%v] = %v\n", i, chunk[i])
+			//Distribute remainders 1 at a time
+			if remainder > 0 {
+				chunk[i].End++
+				chunk[i].Size++
+				remainder--
+			}
+		} else {
+			chunk[i].Size = avgChunkSize
+			chunk[i].Start = chunk[i-1].End + 1
+			chunk[i].End = i*avgChunkSize + avgChunkSize
+			fmt.Printf("Chunk[%v] = %v\n", i, chunk[i])
+			//Distribute remainders 1 at a time
+			if remainder > 0 {
+				chunk[i].End++
+				chunk[i].Size++
+				remainder--
+			}
+		}
+	}
+	return chunk
 }
--- work_queue/work_queue.go
+++ work_queue/work_queue.go
@@ -14,12 +14,9 @@ type WorkQueue struct {
 // Create a new work queue capable of doing nWorkers simultaneous tasks, expecting to queue maxJobs tasks.
 func Create(nWorkers uint, maxJobs uint) *WorkQueue {
 	q := new(WorkQueue)
+	q.NumWorkers = nWorkers
 	// TODO: initialize struct; start nWorkers workers as goroutines
-	for i:=0;i<nWorkers;i++{
-		go func() {
-			
-		}
-	}
+	
 	return q
 }
 
@@ -29,16 +26,27 @@ func (queue WorkQueue) worker() {
 	// Run tasks from the Jobs channel, unless we have been asked to stop.
 	for running {
 		// TODO: listen on the .Jobs channel for incoming tasks
+
 		// TODO: run tasks by calling .Run()
+
 		// TODO: send the return value back on Results channel
+
 		// TODO: exit (return) when a signal is sent on StopRequests
+		stopReq := <-queue.StopRequests
+		if stopReq != 0 {
+			running = false
+			queue.Shutdown()
+			return
+		}
 	}
 }
 
 func (queue WorkQueue) Enqueue(work Worker) {
 	// TODO: put the work into the Jobs channel so a worker can find it and start the task.
+	queue.Jobs <- work
 }
 
 func (queue WorkQueue) Shutdown() {
 	// TODO: tell workers to stop processing tasks.
+
 }
